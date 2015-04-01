47d3e5fbb350b2cbea9ab3e29e8d653b6d056259
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2015-03-31 21:33:49 -0400

    Fixed copy/paste error in coverage script.

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,2 +1,2 @@
-$artifactLocation = 'artifacts\bin\Nito.AsyncEx.Tasks\Debug\net46'
+$artifactLocation = 'artifacts\bin\Nito.AsyncEx.Coordination\Debug\net46'
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/master/Coverage.ps1'))
